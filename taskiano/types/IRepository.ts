2fc352165c111f968f24c95fbf0697d6c3f2f577
ZauJulio <zauhdf@gmail.com>
2022-02-08 20:24:41 -0300

    :label: Add new arq types

---
+++ taskiano/types/IRepository.ts
@@ -0,0 +1,31 @@
+import { Timestamp } from "firebase/firestore";
+import { SchemaOf } from "yup";
+
+export interface IRepositoryConstructor {
+  ref: any;
+  schema: SchemaOf<any>;
+  _name: string;
+}
+
+export type WhereFilterOp =
+  | "<"
+  | "<="
+  | "=="
+  | "!="
+  | ">="
+  | ">"
+  | "array-contains"
+  | "in"
+  | "array-contains-any"
+  | "not-in";
+
+export interface IRepository<T> {
+  index(_id: string): Promise<T[]>;
+  get(_id: any): Promise<T | undefined>;
+  delete(_id: any): Promise<void>;
+  create(data: T, id?: string): Promise<T>;
+  update(_id: any, data: T): Promise<T | undefined>;
+  filter(prop: string, op: WhereFilterOp, value: any): Promise<T[]>;
+  Validator(data: T, schema?: SchemaOf<any>): Promise<void>;
+  castDate(date?: Timestamp | Date | string | null): Date | null | undefined;
+}

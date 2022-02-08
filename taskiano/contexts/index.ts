934ee495cdc0960e64a3f0ac510ce5d6dbb3917a
ZauJulio <zauhdf@gmail.com>
2022-02-08 20:50:36 -0300

    :art: Export all hooks

---
+++ taskiano/contexts/index.ts
@@ -0,0 +1,11 @@
+import { AuthContextProvider } from "./AuthContext";
+import { HistoryContextProvider } from "./HistoryContext";
+import { ProjectsContextProvider } from "./ProjectsContext";
+import { TasksContextProvider } from "./TasksContext";
+
+export {
+  AuthContextProvider,
+  ProjectsContextProvider,
+  TasksContextProvider,
+  HistoryContextProvider,
+};

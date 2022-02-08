8232fe438a9e8b9f092772391394173ad0517ab2
ZauJulio <zauhdf@gmail.com>
2022-02-08 20:49:36 -0300

    :art: Export all hooks

--- taskiano/hooks/index.tsx
+++ taskiano/hooks/index.tsx
@@ -1,7 +1,17 @@
-import { useAuth } from "./useAuth";
+import useAuthHook from "./useAuthHook";
 import useFirebaseAuth from "./useFirebaseAuth";
+import { useAuth } from "./useAuth";
+import { useTasks } from "./useTasks";
 import { useHistory } from "./useHistory";
 import { useProjects } from "./useProjects";
-import { useTasks } from "./useTasks";
+import { useRouterLoading } from "./useRouterLoading";
 
-export { useAuth, useHistory, useProjects, useTasks, useFirebaseAuth };
+export {
+  useAuthHook,
+  useAuth,
+  useHistory,
+  useProjects,
+  useTasks,
+  useFirebaseAuth,
+  useRouterLoading,
+};

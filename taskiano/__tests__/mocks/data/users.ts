86e8fdba61b7001acd4bb56241fce3de5cf82f70
ZauJulio <zauhdf@gmail.com>
2022-02-09 00:25:04 -0300

    :clown_face: Add data mock

---
+++ taskiano/__tests__/mocks/data/users.ts
@@ -0,0 +1,10 @@
+import { IUser } from '../../../types'
+
+export let users: IUser[] = [
+  {
+    id: '7fd18545-8054-45f5-9d76-9addd179d13b',
+    username: 'taciano',
+    avatar: 'https://github.com/tacianosilva.png',
+    email: 'taciano.silva@ufrn.br'
+  }
+]

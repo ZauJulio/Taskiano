46af2a3684209f3b24e3fd78e47a0b475b0b1f20
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-02-09 16:04:48 -0300

    Teste unitario do schema de usuário

---
+++ taskiano/__tests__/test/schemas/UserSchema.test.ts
@@ -0,0 +1,36 @@
+import {
+  UserSchema
+} from '../../../lib/schemas'
+
+describe('user schema', () => {
+  it('should validate the user schema', async () => {
+    const data = {
+      id: '7fd18545-8054-45f5-9d76-9addd179d13b',
+      username: 'taciano',
+      avatar: 'https://github.com/tacianosilva.png',
+      email: 'taciano.silva@ufrn.br'
+    }
+
+    const schema = await UserSchema.validate(data, { abortEarly: false })
+
+    expect(schema).toBeDefined()
+    expect(typeof schema.id).toBe('string')
+    expect(typeof schema.username).toBe('string')
+    expect(typeof schema.avatar).toBe('string')
+    expect(typeof schema.email).toBe('string')
+  })
+
+  it('should fail to validate user schema', async () => {
+    const data = {
+      id: '7fd18545-8054-45f5-9d76-9addd179d13b',
+      avatar: 'https://github.com/tacianosilva.png',
+      email: 'taciano.silva@ufrn.br'
+    }
+    try {
+      await UserSchema.validate(data, { abortEarly: false })
+    } catch (error: any) {
+      expect(error.message).toBe('username is a required field')
+    }
+
+  })
+})
